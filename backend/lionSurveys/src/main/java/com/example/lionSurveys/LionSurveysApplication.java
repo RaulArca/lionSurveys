9f3665160a132b7d534ed68f06fcf7b55461608a
Raul Arca <uo270587@uniovi.es>
2025-03-13 12:18:47 +0100

    first commit
    add the mvc tree

---
+++ backend/lionSurveys/src/main/java/com/example/lionSurveys/LionSurveysApplication.java
@@ -0,0 +1,13 @@
+package com.example.lionSurveys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionSurveysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LionSurveysApplication.class, args);
+	}
+
+}
---
+++ backend/lionSurveys/src/test/java/com/example/lionSurveys/LionSurveysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lionSurveys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionSurveysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
